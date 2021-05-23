605c759bcb6bb634a045355a8d772ac2b230c281
Bartosz Jędrzejewski-Emche <jedrek2603@gmail.com>
2021-05-23 12:08:46 +0200

    Fix the image error

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -24,7 +24,7 @@ class List extends React.Component {
     const {title, image, description, columns} = this.props;
     return (
       <section className={styles.component}>
-        <Hero titleText={title} image={image} />
+        <Hero titleText={title} imageText={image} />
         <div className={styles.description}>
           {ReactHtmlParser(description)}
         </div>
